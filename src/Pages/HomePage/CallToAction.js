80dc7be198b7fdb00fd952559b4584b6679525a2
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2023-02-04 02:41:53 +0600

    some errors occrued on social

--- src/Pages/HomePage/CallToAction.js
+++ src/Pages/HomePage/CallToAction.js
@@ -3,12 +3,14 @@ import { FaArrowAltCircleRight } from 'react-icons/fa';
 
 const CallToAction = () => {
     return (
-        <section className="w-4/5 py-6 dark:bg-indigo-400 dark:text-gray-900 mx-auto -mt-8 h-auto">
+        <section className="w-4/5 py-6 bg-blue-500 dark:text-gray-900 mx-auto -mt-2 h-auto">
             <div className="container mx-auto flex flex-col items-center justify-center p-4 space-y-8 md:p-10 md:px-24 xl:px-48">
                 <h1 className="text-3xl font-bold text-center">Let’s Create Big Stories Together</h1>
                 <p className="pt-2 pb-8 text-xl font-medium text-center">Mobile is in our nerves. We don’t just build apps, we create brand. <br />Choosing us will be your best decision</p>
                 <button className="px-8 py-3 text-lg font-semibold rounded bg-red-600 dark:text-gray-50">
-                    Get A Quotes <span><FaArrowAltCircleRight className='text-center'></FaArrowAltCircleRight></span>
+                    <div className='flex items-center ml-3'>
+                    Get A Quotes <span><FaArrowAltCircleRight className='text-center ml-3'></FaArrowAltCircleRight></span>
+                    </div>
 
                 </button>
             </div>
--- src/Pages/HomePage/Social.js
+++ src/Pages/HomePage/Social.js
@@ -3,14 +3,11 @@ import React from 'react';
 const Social = () => {
     return (
 
-        <div className="bg-black container flex flex-col p-4 mx-auto md:p-8 lg:flex-row divide-gray-400 overflow-hidden rounded">
-            <ul className="self-center py-6 space-y-4 text-center sm:flex sm:space-y-0 sm:justify-around sm:space-x-4 lg:flex-1 lg:justify-start">
-                <li>Shop</li>
-                <li>About</li>
-                <li>Blog</li>
-                <li>Pricing</li>
-                <li>Contact</li>
-            </ul>
+        <div className=" container flex flex-col p-4 mx-auto md:p-8 lg:flex-row divide-gray-400 overflow-hidden rounded">
+            <div className="self-center py-6 space-y-4 text-center sm:flex sm:space-y-0 sm:justify-around sm:space-x-4 lg:flex-1 lg:justify-start">
+                <h1 className='text-3xl font-bold'>Social Media</h1> <br />
+              
+            </div>
             <div className="flex flex-col justify-center pt-6 lg:pt-0">
                 <div className="flex justify-center space-x-4">
                     <a rel="noopener noreferrer" href="#" title="Instagram" className="flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 dark:bg-indigo-400 dark:text-gray-900">
